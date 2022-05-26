e6f69051a9ba6016ed47ace105c650631cad44ab
TayeburRahman <tayebrayhan101@gmail.com>
2022-05-26 13:32:21 +0600

    update

--- src/Components/SignUpSignInPage/FirebaseConfig/FirebaseConfig.js
+++ src/Components/SignUpSignInPage/FirebaseConfig/FirebaseConfig.js
@@ -1,17 +1,13 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_API_KEY,
-  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_APP_ID
-  apiKey: "AIzaSyCYzjOuQm5f4RmpXR6mNYb_3TwPpxnhIFk",
-  authDomain: "travels-10.firebaseapp.com",
-  projectId: "travels-10",
-  storageBucket: "travels-10.appspot.com",
-  messagingSenderId: "225563248491",
-  appId: "1:225563248491:web:b7192eeed2bccb8818ee2e",
-  measurementId: "G-CGSLKMM37K"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+
+ 
+   
 };
 export default firebaseConfig;
